4b5af040a822d2249638665059cba29bec38ee82
Douglas Mezuraro <douglasmez@gmail.com>
2021-05-04 11:18:18 -0300

    Iniciado módulo de navegação

---
+++ react-router/navigation/src/index.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <Fragment>
+        TODO: Inject app here.
+    </Fragment>,
+    document.getElementById('root')
+);
