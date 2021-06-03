0fffe6989b0afd76f4d4cca6d1afb1a72e931f3b
Douglas Mezuraro <douglasmez@gmail.com>
2021-06-03 16:51:18 -0300

    Hooks: Desafio useReducer

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -1,5 +1,6 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useRef } from 'react';
 import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 
 const initialState = {
     number: 0,
@@ -10,8 +11,16 @@ const reducer = (state, action) => {
     switch (action.type) {
         case 'login':
             return { ...state, user: action.payload.user };
-        case 'add':
-            return { ...state, number: state.number + 1 };
+        case 'add_2':
+            return { ...state, number: state.number + 2 };
+        case 'add_n':
+            return { ...state, number: parseInt(state.number) + parseInt(action.payload.n) }
+        case 'mult_7':
+            return { ...state, number: state.number * 7 };
+        case 'div_25':
+            return { ...state, number: state.number / 25 };
+        case 'parse_int':
+            return { ...state, number: parseInt(state.number) };
         default:
             return state;
     };
@@ -20,17 +29,43 @@ const reducer = (state, action) => {
 const UseReducer = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    const editNumber = useRef(null);
+    const editText = useRef(null);
+
     return (
         <div className='UseReducer'>
             <PageTitle title='Hook UseReducer' subtitle='Uma outra forma de ter estado em componentes funcionais!'>
-                <div className='center'>
-                    <span className='text'>{state.user ? state.user : 'Faça login'}</span>
-                    <span className='text'>{state.number}</span>
+                <SectionTitle title='Valores atuais'>
                     <div>
-                        <button className='btn' onClick={() => dispatch({ type: 'login', payload: { user: 'Meza' } })}>Login</button>
-                        <button className='btn' onClick={() => dispatch({ type: 'add' })}>+1</button>
+                        <span className='text'>Usuário: {state.user ? state.user : 'Faça login'}</span>
                     </div>
-                </div>
+                    <div>
+                        <span className='text'>Valor: {state.number}</span>
+                    </div>
+                </SectionTitle>
+
+                <SectionTitle title='Logar'>
+                    <div>
+                        <input className='input' type='text' ref={editText} />
+                        <button className='btn' onClick={() => dispatch({ type: 'login', payload: { user: editText.current.value } })}>Login</button>
+                    </div>
+                </SectionTitle>
+
+                <SectionTitle title='Adicionar um número qualquer'>
+                    <div>
+                        <input className='input' type='number' ref={editNumber} />
+                        <button className='btn' onClick={() => dispatch({ type: 'add_n', payload: { n: editNumber.current.value } })}>Add N</button>
+                    </div>
+                </SectionTitle>
+
+                <SectionTitle title='Outras opções'>
+                    <div>
+                        <button className='btn' onClick={() => dispatch({ type: 'add_2' })}>+2</button>
+                        <button className='btn' onClick={() => dispatch({ type: 'mult_7' })}>*7</button>
+                        <button className='btn' onClick={() => dispatch({ type: 'div_25' })}>/25</button>
+                        <button className='btn' onClick={() => dispatch({ type: 'parse_int' })}>Parse Int</button>
+                    </div>
+                </SectionTitle>
             </PageTitle>
         </div>
     );
