52374ff9fb229c291752726085fa1809e6f16a56
Douglas Mezuraro <douglasmez@gmail.com>
2021-06-06 18:01:55 -0300

    TodoApp - Backend - Configurando o servidor com o Express

---
+++ todo-app/backend/src/config/server.js
@@ -0,0 +1,8 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const port = 3003;
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.listen(port, () => console.log(`Backend is running on ${port}.`));
