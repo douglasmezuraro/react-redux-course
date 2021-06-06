23732da986ef9f903f953be990969dd1e3027f14
Douglas Mezuraro <douglasmez@gmail.com>
2021-06-06 18:34:53 -0300

    TodoApp - Backend - Mapeamento das rotas

--- todo-app/backend/src/api/todo/todo.js
+++ todo-app/backend/src/api/todo/todo.js
@@ -3,7 +3,7 @@ const mongoose = restful.mongoose;
 
 const todoSchema = new mongoose.Schema({
     description: { type: String, required: true },
-    done: { type: Boolean, required: true, default: False },
+    done: { type: Boolean, required: true, default: false },
     createdAt: { type: Date, required: true, default: Date.now }
 });
 
--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -6,3 +6,5 @@ const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.listen(port, () => console.log(`Backend is running on ${port}.`));
+
+module.exports = server;
