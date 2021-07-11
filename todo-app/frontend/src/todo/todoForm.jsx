d6fb350dd88eb3390cf0cf724364a059000269c1
Douglas Mezuraro <douglasmez@gmail.com>
2021-07-11 20:16:45 -0300

    TodoApp - Frontend - Evento adicionar

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
-const TodoForm = () => (
+const TodoForm = props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
             <input id='description' className='form-control' placeholder='Adicione uma tarefa' />
         </Grid>
 
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus' hide={true}/>
+            <IconButton style='primary' icon='plus' hide={false} onClick={props.handleAdd}/>
         </Grid>
     </div>
 );
