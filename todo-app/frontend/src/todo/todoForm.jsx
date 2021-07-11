4162bccd59ea49da203cebaf2e59366e2701177e
Douglas Mezuraro <douglasmez@gmail.com>
2021-07-11 20:35:45 -0300

    TodoApp - Frontend - Evento onChange

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -6,11 +6,12 @@ import IconButton from '../template/iconButton';
 const TodoForm = props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa' />
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa'
+                value={props.description} onChange={props.handleChange} />
         </Grid>
 
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus' hide={false} onClick={props.handleAdd}/>
+            <IconButton style='primary' icon='plus' hide={false} onClick={props.handleAdd} />
         </Grid>
     </div>
 );
