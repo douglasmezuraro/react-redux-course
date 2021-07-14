93c8eedf26e3f8536bf9d9f45193485da2d491dc
Douglas Mezuraro <douglasmez@gmail.com>
2021-07-14 18:02:51 -0300

    Gerenciamento de estado com Redux - Componente Card

--- simple-redux/src/App.js
+++ simple-redux/src/App.js
@@ -4,8 +4,15 @@ import Card from './components/Card';
 const App = () => (
     <div className='App'>
         <h1>Exercício React-Redux (Simples)</h1>
-        <div className='line'>
-            <Card />
+        <div>
+            <div className='row'>
+                <Card blue title='Card #01' chidren='blablabla' />
+                <Card purple title='Card #02' chidren='blablabla' />
+            </div>
+            <div className='row'>
+                <Card green title='Card #03' chidren='blablabla' />
+                <Card red title='Card #04' chidren='blablabla' />
+            </div>
         </div>
     </div>
 );
--- simple-redux/src/components/Card.jsx
+++ simple-redux/src/components/Card.jsx
@@ -2,10 +2,37 @@ import './Card.css'
 
 import React from 'react';
 
+const getColor = props => {
+    if (props.blue) {
+        return 'Blue';
+    };
+
+    if (props.green) {
+        return 'Green';
+    }
+
+    if (props.purple) {
+        return 'Purple';
+    }
+
+    if (props.red) {
+        return 'Red';
+    };
+
+    return 'TODO';
+};
+
 const Card = props => (
-    <div className='card'>
-        Card
-    </div>
+    <div className={`Card ${getColor(props)}`} >
+        <div className='Header'>
+            <span className='Title'>
+                {props.title}
+            </span>
+        </div>
+        <div className='Content'>
+            {props.chidren}
+        </div>
+    </div >
 );
 
 export default Card;
