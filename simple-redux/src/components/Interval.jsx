46c2e88f4f9e0cd8c4cb5a189f66e33ca4be1f1f
Douglas Mezuraro <douglasmez@gmail.com>
2021-07-15 12:18:28 -0300

    Gerenciamento de estado com Redux - Melhorias

--- simple-redux/src/components/Interval.jsx
+++ simple-redux/src/components/Interval.jsx
@@ -1,24 +1,25 @@
 import './Interval.css';
 
 import React from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import Card from './Card';
-import { updateMin, updateMax } from '../store/actions/numbers';
+import * as NumbersActions from '../store/actions/numbers';
 
 const Interval = props => {
-    const { min, max } = props;
+    const { min, updateMin, max, updateMax } = props;
 
     return (
         <Card blue title='Intervalo de Números'>
             <div className='Interval'>
                 <span>
                     <strong>Mínimo:</strong>
-                    <input type='number' step={1} value={min} onChange={e => props.onUpdateMin(+e.target.value)} />
+                    <input type='number' step={1} value={min} onChange={e => updateMin(+e.target.value)} />
                 </span>
                 <span>
                     <strong>Máximo:</strong>
-                    <input type='number' step={1} value={max} onChange={e => props.onUpdateMax(+e.target.value)} />
+                    <input type='number' step={1} value={max} onChange={e => updateMax(+e.target.value)} />
                 </span>
             </div>
         </Card>
@@ -32,15 +33,6 @@ const mapStateToProps = state => (
     }
 );
 
-const mapDispatchToProps = dispatch => (
-    {
-        onUpdateMin(min) {
-            dispatch(updateMin(min));
-        },
-        onUpdateMax(max) {
-            dispatch(updateMax(max));
-        },
-    }
-);
+const mapDispatchToProps = dispatch => bindActionCreators(NumbersActions, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Interval);
