bca6ec60978c193b33779ce16f123aa052170994
Douglas Mezuraro <douglasmez@gmail.com>
2021-07-18 20:39:33 -0300

    MyMoneyApp - Backend - Configurando o Servidor com o Express

---
+++ my-money-app/backend/src/config/server.js
@@ -0,0 +1,10 @@
+const PORT = 3003;
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json())
+
+server.listen(PORT, () => console.log(`Backend is running on port ${PORT}.`));
