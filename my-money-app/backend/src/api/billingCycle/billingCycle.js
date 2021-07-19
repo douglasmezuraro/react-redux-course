03f457878d3f29470db14a9533180881fc2cd765
Douglas Mezuraro <douglasmez@gmail.com>
2021-07-19 11:29:25 -0300

    MyMoneyApp - Backend - Testando a api (postman)/mensagens de validações

--- my-money-app/backend/src/api/billingCycle/billingCycle.js
+++ my-money-app/backend/src/api/billingCycle/billingCycle.js
@@ -3,24 +3,24 @@ const mongoose = restful.mongoose;
 
 const creditSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true },
-        value: { type: Number, min: 0, required: true },
+        name: { type: String, required: [true, 'Informe o nome do crédito.'] },
+        value: { type: Number, min: 0, required: [true, 'Informe o valor do crédito.'] },
     }
 );
 
 const debtSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true },
-        value: { type: Number, min: 0, required: true },
+        name: { type: String, required: [true, 'Informe o nome do débito.'] },
+        value: { type: Number, min: 0, required: [true, 'Informe o valor do débito.'] },
         status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO'] },
     }
 );
 
 const billingCycleSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true },
-        month: { type: Number, min: 1, max: 12, required: true },
-        year: { type: Number, min: 1900, max: 2100, required: true },
+        name: { type: String, required: [true, 'Informe o nome do ciclo de pagamento.'] },
+        month: { type: Number, min: 1, max: 12, required: [true, 'Informe o mês do ciclo de pagamento.'] },
+        year: { type: Number, min: 1900, max: 2100, required: [true, 'Informe o ano do ciclo de pagamento.'] },
         credits: [creditSchema],
         debts: [debtSchema],
     }
