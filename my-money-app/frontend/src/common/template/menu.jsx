cdfe979af9681aa5d3189db1de34cda2cba7f5e2
Douglas Mezuraro <douglasmez@gmail.com>
2021-08-16 17:41:43 -0300

    MyMoneyApp - Frontend - Componente menu (parte 1)

---
+++ my-money-app/frontend/src/common/template/menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Menu = () => (
+    <ul className='sidebar-menu'>
+        SideBar
+    </ul>
+);
+
+export default Menu;
--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -1,10 +1,14 @@
 import '../common/template/dependencies';
+
 import React from 'react';
+
 import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar';
 
 const App = () => (
     <div className='wrapper'>
         <Header />
+        <SideBar />
     </div>
 );
 
