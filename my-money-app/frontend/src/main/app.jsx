1b72f69c9c1aa4e7f96fb491200bfd98c3a3da1c
Douglas Mezuraro <douglasmez@gmail.com>
2021-08-16 17:34:20 -0300

    MyMoneyApp - Frontend - Componente cabeçalho

--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -1,11 +1,10 @@
 import '../common/template/dependencies';
 import React from 'react';
+import Header from '../common/template/header'
 
 const App = () => (
     <div className='wrapper'>
-        <h1>
-            App
-        </h1>
+        <Header />
     </div>
 );
 
