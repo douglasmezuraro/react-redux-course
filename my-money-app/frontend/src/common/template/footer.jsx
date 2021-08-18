1836224a197389811ca7c1b4e99c5b1abc8301cc
Douglas Mezuraro <douglasmez@gmail.com>
2021-08-17 23:14:43 -0300

    MyMoneyApp - Frontend - Componente cabeçalho

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className='main-footer'>
+        <strong>Copyright &copy; {new Date().getFullYear()}</strong>
+        <a href='https://github.com/douglasmezuraro/' target='_blank'> Douglas Mezuraro</a>
+    </footer>
+);
+
+export default Footer;
--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -4,11 +4,16 @@ import React from 'react';
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar';
+import Footer from '../common/template/footer';
 
 const App = () => (
     <div className='wrapper'>
         <Header />
         <SideBar />
+        <div className='content-wrapper'>
+            <h1>Conteúdo</h1>
+        </div>
+        <Footer />
     </div>
 );
 
