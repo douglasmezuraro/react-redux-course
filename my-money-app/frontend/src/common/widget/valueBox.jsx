81f71909d0c09e79f1a0887ad93b438de27cc3b7
Douglas Mezuraro <douglasmez@gmail.com>
2021-08-17 23:58:54 -0300

    MyMoneyApp - Frontend - Componente ValueBox

---
+++ my-money-app/frontend/src/common/widget/valueBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Grid from '../layout/grid';
+
+const ValueBox = ({ color, cols, icon, text, value }) => (
+    <Grid cols={cols}>
+        <div className={`small-box bg-${color}`}>
+            <div className='inner'>
+                <h3>{value}</h3>
+                <p>{text}</p>
+            </div>
+
+            <div className='icon'>
+                <i className={icon} />
+            </div>
+        </div>
+    </Grid>
+);
+
+export default ValueBox;
