9268c8f22c7b1fd2c747f1e34f6735bf9f32d14d
Douglas Mezuraro <douglasmez@gmail.com>
2021-08-20 18:16:47 -0300

    MyMoneyApp - Frontend - Componente BillingCycle: Conectando com Redux

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import ContentHeader from '../common/template/contentHeader';
 import Content from '../common/template/content';
@@ -8,7 +10,13 @@ import TabsHeader from '../common/tab/tabsHeader';
 import TabsContent from '../common/tab/tabsContent';
 import TabHeader from '../common/tab/tabHeader';
 import TabContent from '../common/tab/tabContent';
+
+import { selectTab } from '../common/tab/actions';
 class BillingCycle extends Component {
+    componentDidMount() {
+        this.props.selectTab('tabList');
+    };
+
     render() {
         return (
             <div>
@@ -35,4 +43,6 @@ class BillingCycle extends Component {
     };
 };
 
-export default BillingCycle;
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch);
+
+export default connect(null, mapDispatchToProps)(BillingCycle);
