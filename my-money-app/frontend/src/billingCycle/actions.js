b655116f0cccf74e2de5de4071b4717b330cb063
Douglas Mezuraro <douglasmez@gmail.com>
2021-08-23 10:34:09 -0300

    MyMoneyApp - Frontend - Melhorias na inclusão do ciclo de pagamento

--- my-money-app/frontend/src/billingCycle/actions.js
+++ my-money-app/frontend/src/billingCycle/actions.js
@@ -1,5 +1,8 @@
 import Axios from 'axios';
 import { toastr } from 'react-redux-toastr';
+import { reset as resetForm } from 'redux-form';
+
+import { selectTab, showTabs } from '../common/tab/actions';
 
 const BASE_URL = 'http://localhost:3003/api';
 
@@ -10,13 +13,15 @@ export const getList = () => (
     }
 );
 
-export const create = values => {
-    Axios.post(`${BASE_URL}/billingCycles/`, values)
-        .then(() => toastr.success('Sucesso', 'Operação realizada com sucesso.'))
+export const create = (values) =>
+    dispatch => Axios.post(`${BASE_URL}/billingCycles/`, values)
+        .then(() => {
+            toastr.success('Sucesso', 'Operação realizada com sucesso.');
+            dispatch([
+                resetForm('billingCycleForm'),
+                getList(),
+                selectTab('tabList'),
+                showTabs('tabList', 'tabCreate'),
+            ]);
+        })
         .catch(e => e.response.data.errors.forEach(error => toastr.error('Erro', error)));
-
-    return {
-        type: 'BILLING_CYCLE_CREATED',
-        payload: null,
-    };
-};
