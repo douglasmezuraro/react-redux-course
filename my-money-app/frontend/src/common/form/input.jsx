afb30ea594cd1ef69bc5b0a769897e8b56731eaf
Douglas Mezuraro <douglasmez@gmail.com>
2021-09-07 19:56:35 -0300

    MyMoneyApp - FrontEnd - Adicionado TODO no componente de input

--- my-money-app/frontend/src/common/form/input.jsx
+++ my-money-app/frontend/src/common/form/input.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+//TODO: Adicionar parametrização para formatar como moeda, exemplo: http://jsfiddle.net/trixta/UC6tG/
+
 const Input = ({ input, placeholder, readOnly, step, type }) => (
     <input {...input} className='form-control' placeholder={placeholder} readOnly={readOnly} step={step} type={type} />
 );
