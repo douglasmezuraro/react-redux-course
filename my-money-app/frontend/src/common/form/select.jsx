5b562de762e41f304e3bdf626396ff3420253b41
Douglas Mezuraro <douglasmez@gmail.com>
2021-09-08 09:13:06 -0300

    MyMoneyApp - FrontEnd - Adicionado opção vazia no início dos componentes de seleção

--- my-money-app/frontend/src/common/form/select.jsx
+++ my-money-app/frontend/src/common/form/select.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Select = ({ input, options, placeholder, readOnly }) => (
     <select {...input} className='form-control' placeholder={placeholder} readOnly={readOnly}>
+        <option />
         {options.map((option, index) => <option key={index} value={option.value}>{option.label}</option>)}
     </select>        
 );
